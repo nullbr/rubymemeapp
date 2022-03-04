b40672330773a6747c81220070002d9f82a89ab2
brunoleite <bmarianoleite3@gmail.com>
2022-03-04 18:14:17 +0000

    added db content to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Meme.create! ([
+    {
+        name: "Happy Ruby",
+        description: %{ 
+            a happy ruby a happy ruby a happy ruby a happy ruby 
+            a happy ruby a happy ruby a happy ruby a happy ruby
+        }.squish,
+        author: "Bruno",
+        stars: 3,
+        review: "not a greate happy ruby meme"
+    },
+    {
+        name: "Sad Ruby",
+        description: %{ 
+            a sad ruby a sad ruby a sad ruby a sad ruby 
+            a sad ruby a sad ruby a sad ruby a sad ruby
+        }.squish,
+        author: "Bruno",
+        stars: 5,
+        review: "pretty funny this sad meme haha"
+    },
+    {
+        name: "Cool Ruby",
+        description: %{ 
+            a cool ruby a cool ruby a cool ruby a cool ruby 
+            a cool ruby a cool ruby a cool ruby a cool ruby
+        }.squish,
+        author: "Bruno",
+        stars: 5,
+        review: "realy cool ruby meme"
+    },
+    {
+        name: "Good Ruby",
+        description: %{ 
+            a good ruby a good ruby a good ruby a good ruby 
+            a good ruby a good ruby a good ruby a good ruby
+        }.squish,
+        author: "Bruno",
+        stars: 3
+    }
+])
