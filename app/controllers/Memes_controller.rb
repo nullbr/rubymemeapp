8d70ed67cdc5dc29dad3b5af519ab41c9f5f1d47
nullbr <bmarianoleite3@gmail.com>
2022-03-09 11:03:30 -0300

    Added create new functionality and page

--- app/controllers/Memes_controller.rb
+++ app/controllers/Memes_controller.rb
@@ -13,11 +13,25 @@ class MemesController < ApplicationController
 
   def update
     @meme = Meme.find(params[:id])
-    meme_params = 
-      params.require(:meme).
-        permit(:name, :description, :author, :image_url) 
     @meme.update(meme_params)
     redirect_to @meme
+  end
+
+  def new
+    @meme = Meme.new
+  end
+
+  def create
+    @meme = Meme.create(meme_params)
+    @meme.save
+    redirect_to @meme
+  end
+
+private
+
+  def meme_params
+    params.require(:meme).
+        permit(:name, :description, :author, :image_url) 
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
 
   root "memes#index"
 
-  get "memes", to: "memes#index"
-  get "memes/:id", to: "memes#show", as: "meme"
-  get "memes/:id/edit", to: "memes#edit", as: "edit_meme"
-  patch "memes/:id", to: "memes#update"
+  resources :memes
 
 end
