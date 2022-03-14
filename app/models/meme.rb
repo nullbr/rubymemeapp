9c8f064ba8a7d927229958cefc7028ec96b95cd0
nullbr <bmarianoleite3@gmail.com>
2022-03-14 17:30:56 -0300

    added to validations

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -1,7 +1,8 @@
 class Meme < ApplicationRecord
 
-    validates :name, :author, presence: true
-    validates :description, length: { minimum: 25 }
+    validates :name, :author, presence: true && format: { with: /\A[a-zA-Z]+\z/,
+        message: "only allows letters" }
+    validates :description, length: { minimum: 25, maximum: 500 }
     validates :image_file_name, format: {
         with: /\w+\.(jpg|gif|png)\z/i,
         message: "must be a JPG, PNG or GIF image"
