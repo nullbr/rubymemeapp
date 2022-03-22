8c706b4c0b3129b47000ee6c63d7d2018dd48d7e
nullbr <bmarianoleite3@gmail.com>
2022-03-21 22:54:21 -0300

    Reviews add and edit pages

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,47 @@
 class ReviewsController < ApplicationController
+    
+    before_action :set_meme
+
     def index
-        @meme = Meme.find(params[:meme_id])
         @reviews = @meme.reviews
+    end
+
+    def edit
+        @review = @meme.reviews.find(params[:id])
+    end
+
+    def update
+        @review = @meme.reviews.find(params[:id])
+        if @review.update(review_params)
+            redirect_to meme_reviews_path(@meme), notice: "Review updated successfuly"
+        else
+            render :edit
+        end
+    end
+    
+    def new
+        @review = @meme.reviews.new
+    end
+
+    def create
+    
+        @review = @meme.reviews.new(review_params)
+        
+        if @review.save
+            redirect_to meme_reviews_path(@meme), notice: "Review submitted"
+        else
+            render :new
+        end
+    end
+
+private
+
+    def review_params
+        params.require(:review).
+            permit(:username, :email, :review, :stars)
+    end
+
+    def set_meme
+        @meme = Meme.find(params[:meme_id])
     end
 end
