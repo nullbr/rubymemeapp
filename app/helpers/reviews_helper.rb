38172934c20a4f4adc985556cdb9babf42a9c44d
nullbr <bmarianoleite3@gmail.com>
2022-03-21 22:54:47 -0300

    reviews edit and add new pages

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,9 @@
 module ReviewsHelper
+    def day_month(review)
+        if review.blank?
+            "no date"
+        else
+            review.updated_at.strftime("%d %b %Y")
+        end
+    end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -4,8 +4,9 @@ class Review < ApplicationRecord
   validates :username, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :review, length: { minimum: 25, maximum: 500 }
-  validates :stars, numericality: { 
-    only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 
-  }
+  
+  STARS_OPTIONS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  
+  validates :stars, inclusion: { in: STARS_OPTIONS }
 
 end
