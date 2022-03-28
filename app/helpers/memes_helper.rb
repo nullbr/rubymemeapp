fea7a9e49b9fde956684be7e777d397ee27e8a72
nullbr <bmarianoleie3@gmail.com>
2022-03-28 20:54:29 +0000

    added avg reviews stars to show page

--- app/helpers/memes_helper.rb
+++ app/helpers/memes_helper.rb
@@ -1,23 +1,46 @@
 module MemesHelper
+  def date(meme)
+    meme.created_at.strftime('%B %d, %Y')
+  end
 
-    def date(meme)
-        meme.created_at.strftime("%B %d, %Y")
+  def day_month(meme)
+    if meme.blank?
+      'no date'
+    else
+      meme.updated_at.strftime('%d %b %Y')
+    end
+  end
+
+  def discardpath(meme)
+    if meme.id.nil?
+      root_path
+    else
+      meme_path(meme)
+    end
+  end
+
+	def avg_stars(meme)
+    sum = 0
+    meme.reviews.each do |review|
+      sum += review.stars.to_i
     end
 
-    def day_month(meme)
-        if meme.blank?
-            "no date"
-        else
-            meme.updated_at.strftime("%d %b %Y")
-        end
-    end
+    sum / meme.reviews.size
+	end
 
-    def discardpath(meme)
-        if meme.id.nil?
-            root_path
-        else
-            meme_path(meme)
-        end
+  def checked_stars(meme)
+    if meme.reviews.size.zero?
+      0
+    else
+      avg_stars(meme).to_i
     end
+  end
 
+  def unchecked_stars(meme)
+    if meme.reviews.size.zero?
+      0
+    else
+      5 - avg_stars(meme).to_i
+    end
+  end
 end
--- app/models/meme.rb
+++ app/models/meme.rb
@@ -1,22 +1,20 @@
 class Meme < ApplicationRecord
+  has_many :reviews
 
-    has_many :reviews
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :author, format: { with: /\A[a-zA-Z]+\z/,
+                               message: 'only allows letters' }
+  validates :description, length: { minimum: 25, maximum: 500 }
+  validates :image_file_name, format: {
+    with: /\w+\.(jpg|gif|png)\z/i,
+    message: 'must be a JPG, PNG or GIF image'
+  }
 
-    validates :name, presence: true, length: {maximum: 15}
-    validates :author, format: { with: /\A[a-zA-Z]+\z/,
-        message: "only allows letters" }
-    validates :description, length: { minimum: 25, maximum: 500 }
-    validates :image_file_name, format: {
-        with: /\w+\.(jpg|gif|png)\z/i,
-        message: "must be a JPG, PNG or GIF image"
-    }
-    
-    def self.descorder
-        order("updated_at desc")
-    end
-    
-    def no_review?
-        review.blank? || stars.blank?
-    end
+  def self.descorder
+    order('updated_at desc')
+  end
 
+  def no_review?
+    review.blank? || stars.blank?
+  end
 end
