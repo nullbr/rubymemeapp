01e6263ccac889c6be11ffcc3716b0a7f76381b3
brunoleite <bmarianoleite3@gmail.com>
2022-03-29 01:59:40 +0000

    fixed mmore erging issues

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,7 +5,7 @@ class Review < ApplicationRecord
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :review, length: { minimum: 25, maximum: 500 }
 
-  STARS_OPTIONS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  STARS_OPTIONS = [1, 2, 3, 4, 5]
 
   validates :stars, inclusion: { in: STARS_OPTIONS }
 end
