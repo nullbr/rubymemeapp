335b7a9bd2793d4c3d29e4ad5709a291b1ea5e3f
brunoleite <bmarianoleite3@gmail.com>
2022-04-22 03:14:51 +0000

    setting up filters

--- app/controllers/Memes_controller.rb
+++ app/controllers/Memes_controller.rb
@@ -3,16 +3,23 @@ class MemesController < ApplicationController
   before_action :require_admin, except: %i[index show new create]
 
   def index
-    @memes = Meme.descorder
+    @memes = case params[:filter]
+             when 'oldest'
+               Meme.ascorder
+             when 'gif'
+               Meme.gif
+             when 'png_or_jpg'
+               Meme.png_or_jpg
+             else
+               Meme.descorder
+             end
   end
 
   def show
     @meme = Meme.find(params[:id])
     @likers = @meme.likers
     @categories = @meme.categories
-    if current_user
-      @like = current_user.likes.find_by(meme_id: @meme.id)
-    end
+    @like = current_user.likes.find_by(meme_id: @meme.id) if current_user
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :categories
   root 'memes#index'
 
+  get 'memes/filter/:filter' => 'memes#index', as: :filtered_memes
+
   resources :memes do
     resources :reviews
     resources :likes, only: [:create, :destroy]
