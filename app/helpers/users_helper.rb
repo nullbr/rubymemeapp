f07a9018ce7bf2cc079c92f87b4a8eb90f7dd637
brunoleite <bmarianoleite3@gmail.com>
2022-04-25 15:49:23 +0000

    added user likes page, and worked on formatting

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,18 @@
 module UsersHelper
-    def pro_or_memeber(user)
-        user.admin_status ? "PRO" : "MEMEBER"
+  def pro_or_memeber(user)
+    user.admin_status ? 'PRO' : 'MEMEBER'
+  end
+
+  def which_page
+    case params[:page]
+    when 'reviews'
+      'reviews'
+    when 'memes'
+      'memes'
+    when 'likes'
+      'likes'
+    else
+      'profile'
     end
+  end
 end
