c01d6bac3a83d27a4b3312574b38d774c1328cb7
Bruno Leite <bmarianoleite3@gmail.com>
2023-04-04 20:45:07 -0300

    fix deploy directory bug

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :application, "ruby_memes"
 set :repo_url, "git@github.com:nullbr/rubymemeapp.git"
 
 # Deploy to the user's home directory
-set :deploy_to, "/home/#{fetch :application}"
+set :deploy_to, "/home/nullbr/#{fetch :application}"
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
